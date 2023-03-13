35c0617242dc403661bbca67b3a8e3bd75cc8343
Court Schuett <schuettc@amazon.com>
2023-03-13 10:09:31 -0500

    fix: update tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,9 +4,98 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { config } from 'dotenv';
 import { Minecraft } from '../src/minecraft';
 
-config();
+config({ path: '../.sample.env' });
 
-const stackProps = {
+const javaStackProps = {
+  minecraftEdition: 'java',
+  serverSubDomain: 'minecraft',
+  domain: 'example.com',
+  hostedZoneId: 'Z00000000000000000000',
+  memorySize: '8192',
+  cpuSize: '4096',
+  snsEmail: 'admin@example.com',
+  startupMin: '10',
+  shutdownMin: '20',
+  debug: 'false',
+};
+
+test('Snapshot', () => {
+  const app = new App();
+  const stack = new Minecraft(app, 'test-java', {
+    ...javaStackProps,
+  });
+
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
+});
+
+const bedrockStackProps = {
+  minecraftEdition: 'bedrock',
+  serverSubDomain: 'minecraft',
+  domain: 'example.com',
+  hostedZoneId: 'Z00000000000000000000',
+  memorySize: '8192',
+  cpuSize: '4096',
+  snsEmail: 'admin@example.com',
+  startupMin: '10',
+  shutdownMin: '20',
+  debug: 'false',
+};
+test('Snapshot', () => {
+  const app = new App();
+  const stack = new Minecraft(app, 'test-bedrock', {
+    ...bedrockStackProps,
+  });
+
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
+});
+
+const noSNSStackProps = {
+  minecraftEdition: 'java',
+  serverSubDomain: 'minecraft',
+  domain: 'example.com',
+  hostedZoneId: 'Z00000000000000000000',
+  memorySize: '8192',
+  cpuSize: '4096',
+  startupMin: '10',
+  shutdownMin: '20',
+  debug: 'true',
+};
+
+test('Snapshot', () => {
+  const app = new App();
+  const stack = new Minecraft(app, 'test-noSNS', {
+    ...noSNSStackProps,
+  });
+
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
+});
+
+const bedrockDebugStackProps = {
+  minecraftEdition: 'bedrock',
+  serverSubDomain: 'minecraft',
+  domain: 'example.com',
+  hostedZoneId: 'Z00000000000000000000',
+  memorySize: '8192',
+  cpuSize: '4096',
+  snsEmail: 'admin@example.com',
+  startupMin: '10',
+  shutdownMin: '20',
+  debug: 'true',
+};
+test('Snapshot', () => {
+  const app = new App();
+  const stack = new Minecraft(app, 'test-bedrock', {
+    ...bedrockDebugStackProps,
+  });
+
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
+});
+
+const envStackProps = {
   minecraftEdition: process.env.MINECRAFT_EDITION || '',
   serverSubDomain: process.env.SERVER_SUBDOMAIN || '',
   domain: process.env.DOMAIN || '',
@@ -21,8 +110,8 @@ const stackProps = {
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new Minecraft(app, 'test', {
-    ...stackProps,
+  const stack = new Minecraft(app, 'envtest', {
+    ...envStackProps,
   });
 
   const template = Template.fromStack(stack);
