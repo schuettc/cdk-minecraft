97586226750068dba3e87d4fc0abda9e8494eaac
Court Schuett <schuettc@amazon.com>
2024-04-19 22:14:43 -0500

    fix: update projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,18 +1,21 @@
 const { awscdk } = require('projen');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.68.0',
+  cdkVersion: '2.134.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-minecraft',
   license: 'Apache-2.0',
   author: 'Court Schuett',
+  projenrcTs: true,
   authorAddress: 'https://subaud.io',
   copyrightOwner: 'Court Schuett',
-  defaultReleaseBranch: 'main',
   appEntrypoint: 'minecraft.ts',
   depsUpgradeOptions: {
     ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
     },
   },
   autoApproveOptions: {
@@ -23,7 +26,6 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   devDeps: ['esbuild'],
   deps: ['dotenv'],
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
-  defaultReleaseBranch: 'main',
 });
 
 const common_exclude = [
@@ -37,6 +39,28 @@ const common_exclude = [
 
 project.addTask('launch', {
   exec: 'yarn && yarn projen && yarn build && yarn cdk bootstrap && yarn cdk deploy',
+});
+
+project.tsconfigDev.file.addOverride('include', [
+  'src/**/*.ts',
+  'site/src/**/*.tsx',
+  './.projenrc.ts',
+]);
+
+project.eslint.addOverride({
+  files: ['site/src/**/*.tsx', 'src/resources/**/*.ts'],
+  rules: {
+    'indent': 'off',
+    '@typescript-eslint/indent': 'off',
+  },
+});
+
+project.eslint.addOverride({
+  files: ['src/resources/**/*.ts', 'src/*.ts', 'site/src/**/*.tsx'],
+  rules: {
+    '@typescript-eslint/no-require-imports': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
 });
 
 project.gitignore.exclude(...common_exclude);
